3b008abf0f8d09dafab243b4e4dfad205beb3db2
yulixz <yuliana.murillo@.ad.unosquare.com>
2016-02-01 11:02:34 -0600

    first commit

---
+++ inputExample/inputExample/scripts/decrement.js
@@ -0,0 +1,47 @@
+﻿
+
+var oranges = 12;
+var pears = 7;
+var apples = 3;
+
+var oranges2 = 0;
+var pears2 = 0;
+var apples2 = 0;
+
+
+function decrementOrangeF() {   
+    if (oranges == 0) {
+        alert("No hay naranjas"); }
+    else {
+        oranges = oranges - 1;
+        oranges2 = oranges2 + 1;
+        document.getElementById("orangesO").value = oranges2;
+        document.getElementById("txOrangesId").value = oranges;
+    }
+  //  document.getElementById('myField').value = product(2, 3);
+}
+
+
+function decrementPearF() {    
+    if (pears == 0) {
+        alert("No hay peras");
+    }
+    else {
+        pears = pears - 1;
+        pears2++;
+        document.getElementById("pearsO").value = pears2;
+        document.getElementById("txPearsId").value = pears;
+    }
+}
+
+function decrementAppleF() {    
+    if (apples == 0) {
+        alert("No hay manzanas");
+    }
+    else {
+        apples = apples - 1;
+        apples2++;
+        document.getElementById("applesO").value = apples2;
+        document.getElementById("txApplesId").value = apples;
+    }
+}
